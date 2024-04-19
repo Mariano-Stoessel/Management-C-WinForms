cae94d080612c841039facf81d3f7e9fc399c8c1
Estrella Berrios <167035833+Estrellaberrios@users.noreply.github.com>
2024-04-19 18:37:13 -0300

    usuario
    datos

--- Management-WinForms/alternativa3/usuario.Designer.cs
+++ Management-WinForms/alternativa3/usuario.Designer.cs
@@ -29,26 +29,260 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtcorreo = new System.Windows.Forms.Label();
+            this.txtdocumento = new System.Windows.Forms.TextBox();
+            this.txtnombrecompleto = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.lblclave = new System.Windows.Forms.Label();
+            this.txtclave = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtconfirmar = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label5 = new System.Windows.Forms.Label();
+            this.documento = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.correoo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.correo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(26)))), ((int)(((byte)(26)))));
+            this.label1.Dock = System.Windows.Forms.DockStyle.Left;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(100, 99);
+            this.label1.Location = new System.Drawing.Point(0, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(109, 39);
+            this.label1.Size = new System.Drawing.Size(309, 637);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label2.Location = new System.Drawing.Point(16, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(150, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Numero de documento :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label3.Location = new System.Drawing.Point(16, 149);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(118, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nombre completo:";
+            // 
+            // txtcorreo
+            // 
+            this.txtcorreo.AutoSize = true;
+            this.txtcorreo.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.txtcorreo.Location = new System.Drawing.Point(16, 214);
+            this.txtcorreo.Name = "txtcorreo";
+            this.txtcorreo.Size = new System.Drawing.Size(51, 16);
+            this.txtcorreo.TabIndex = 3;
+            this.txtcorreo.Text = "Correo:";
+            // 
+            // txtdocumento
+            // 
+            this.txtdocumento.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.txtdocumento.Location = new System.Drawing.Point(15, 109);
+            this.txtdocumento.Name = "txtdocumento";
+            this.txtdocumento.Size = new System.Drawing.Size(232, 22);
+            this.txtdocumento.TabIndex = 4;
+            // 
+            // txtnombrecompleto
+            // 
+            this.txtnombrecompleto.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.txtnombrecompleto.Location = new System.Drawing.Point(16, 177);
+            this.txtnombrecompleto.Name = "txtnombrecompleto";
+            this.txtnombrecompleto.Size = new System.Drawing.Size(231, 22);
+            this.txtnombrecompleto.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.textBox3.Location = new System.Drawing.Point(15, 245);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(231, 22);
+            this.textBox3.TabIndex = 6;
+            // 
+            // lblclave
+            // 
+            this.lblclave.AutoSize = true;
+            this.lblclave.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.lblclave.Location = new System.Drawing.Point(12, 290);
+            this.lblclave.Name = "lblclave";
+            this.lblclave.Size = new System.Drawing.Size(79, 16);
+            this.lblclave.TabIndex = 7;
+            this.lblclave.Text = "Contrasena:";
+            // 
+            // txtclave
+            // 
+            this.txtclave.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.txtclave.Location = new System.Drawing.Point(12, 319);
+            this.txtclave.Name = "txtclave";
+            this.txtclave.Size = new System.Drawing.Size(232, 22);
+            this.txtclave.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label6.Location = new System.Drawing.Point(12, 363);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(137, 16);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Confirmar contrasena:";
+            // 
+            // txtconfirmar
+            // 
+            this.txtconfirmar.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.txtconfirmar.Location = new System.Drawing.Point(12, 405);
+            this.txtconfirmar.Name = "txtconfirmar";
+            this.txtconfirmar.Size = new System.Drawing.Size(232, 22);
+            this.txtconfirmar.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.SystemColors.Control;
+            this.button1.Location = new System.Drawing.Point(31, 490);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(240, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Guardar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.SystemColors.Control;
+            this.button2.Location = new System.Drawing.Point(29, 532);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(240, 23);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Editar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.ForeColor = System.Drawing.SystemColors.Control;
+            this.button3.Location = new System.Drawing.Point(29, 574);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(240, 23);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Eliminar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(278, 29);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "DATOS DEL USUARIO";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.documento,
+            this.correoo,
+            this.correo,
+            this.Column2});
+            this.dataGridView1.Location = new System.Drawing.Point(324, 139);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(610, 374);
+            this.dataGridView1.TabIndex = 15;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(324, 80);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(610, 31);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Lista de Usuarios :";
+            // 
+            // documento
+            // 
+            this.documento.HeaderText = "documento";
+            this.documento.MinimumWidth = 6;
+            this.documento.Name = "documento";
+            this.documento.Width = 125;
+            // 
+            // correoo
+            // 
+            this.correoo.HeaderText = "Nombre completo";
+            this.correoo.MinimumWidth = 6;
+            this.correoo.Name = "correoo";
+            this.correoo.Width = 125;
+            // 
+            // correo
+            // 
+            this.correo.HeaderText = "correo";
+            this.correo.MinimumWidth = 6;
+            this.correo.Name = "correo";
+            this.correo.Width = 125;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Rol";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 125;
             // 
             // usuario
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(61)))), ((int)(((byte)(61)))), ((int)(((byte)(61)))));
+            this.ClientSize = new System.Drawing.Size(946, 637);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtconfirmar);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtclave);
+            this.Controls.Add(this.lblclave);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.txtnombrecompleto);
+            this.Controls.Add(this.txtdocumento);
+            this.Controls.Add(this.txtcorreo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.MaximumSize = new System.Drawing.Size(964, 684);
+            this.MinimumSize = new System.Drawing.Size(964, 684);
             this.Name = "usuario";
             this.Text = "usuario";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -57,5 +291,25 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label txtcorreo;
+        private System.Windows.Forms.TextBox txtdocumento;
+        private System.Windows.Forms.TextBox txtnombrecompleto;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label lblclave;
+        private System.Windows.Forms.TextBox txtclave;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtconfirmar;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn documento;
+        private System.Windows.Forms.DataGridViewTextBoxColumn correoo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn correo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
     }
 }
--- Management-WinForms/alternativa3/usuario.cs
+++ Management-WinForms/alternativa3/usuario.cs
@@ -16,5 +16,25 @@ namespace alternativa3
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
     }
 }
